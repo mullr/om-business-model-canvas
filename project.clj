27b5d742d130911d2e6339cd93e6cc72dcb53b4b
Russell Mull <russell@c2klearning.com>
2013-12-24 01:22:39 +0900

    Set up basic layout

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2127"]
-                 [om "0.1.0-SNAPSHOT"]]
+                 [om "0.1.0-SNAPSHOT"]
+                 [sablono "0.1.1"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
--- src/business_model_canvas/core.cljs
+++ src/business_model_canvas/core.cljs
@@ -1,9 +1,128 @@
 (ns business-model-canvas.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [sablono.core :as html :refer [html] :include-macros true]))
 
-(defn widget [data]
+(set! *print-fn* #(.log js/console %))
+
+(defn header-box [data opts]
   (om/component
-    (dom/div nil "Hello, world!")))
+    (html [:div.header-box
+           [:div.row (:title opts)]
+           [:div.row (-> data :value)]])))
 
-(om/root {} widget js/document.body)
+(defn canvas-cell [data opts]
+  (om/component
+    (html [:div.table-cell
+           [:div (:title opts)]
+           [:ul
+            (for [item data]
+              [:li (:value item)])]])))
+
+(defn root [data]
+  (om/component
+    (html
+      [:div.container
+       [:div.row
+        [:div.col-md-4.title "The Business Model Canvas"]
+
+        [:div.col-md-3 (om/build header-box data {:opts {:title "Designed For:"}
+                                                  :path [:designed-for]})]
+
+        [:div.col-md-3 (om/build header-box data {:opts {:title "Designed By:"}
+                                                  :path [:designed-by]})]
+
+        [:div.col-md-1 (om/build header-box data {:opts {:title "Date:"}
+                                                  :path [:date]})]
+
+        [:div.col-md-1 (om/build header-box data {:opts {:title "Version:"}
+                                                  :path [:version]})]]
+
+       [:div.row
+        [:table.table.table-bordered.canvas-table
+         [:tr
+          [:td {:rowSpan 2, :colSpan 2}
+           (om/build canvas-cell data {:opts {:title "Key Partners"}
+                                       :path [:sections :key-partners]})]
+
+          [:td {:rowSpan 1, :colSpan 2}
+           (om/build canvas-cell data {:opts {:title "Key Activities"}
+                                       :path [:sections :key-activities]})]
+
+          [:td {:rowSpan 2, :colSpan 2}
+           (om/build canvas-cell data {:opts {:title "Value Propositions"}
+                                       :path [:sections :value-propositions]})]
+
+          [:td {:rowSpan 1, :colSpan 2}
+           (om/build canvas-cell data {:opts {:title "Customer Relationships"}
+                                       :path [:sections :customer-relationships]})]
+
+          [:td {:rowSpan 2, :colSpan 2}
+          (om/build canvas-cell data {:opts {:title "Customer Segments"}
+                                       :path [:sections :customer-segments]})]]
+         [:tr
+          [:td {:rowSpan 1, :colSpan 2}
+          (om/build canvas-cell data {:opts {:title "Key Resources"}
+                                       :path [:sections :key-resources]})]
+
+          [:td {:rowSpan 1, :colSpan 2}
+          (om/build canvas-cell data {:opts {:title "Channels"}
+                                       :path [:sections :channels]})]]
+
+         [:tr
+          [:td {:rowSpan 1, :colSpan 5}
+           (om/build canvas-cell data {:opts {:title "Cost Structure"}
+                                       :path [:sections :cost-structure]})]
+
+          [:td {:rowSpan 1, :colSpan 5}
+           (om/build canvas-cell data {:opts {:title "Revenue Streams"}
+                                       :path [:sections :revenue-streams]})]]
+
+         ]]]
+      )))
+
+(def doc
+  (atom
+    {:designed-for {:value "A customer"}
+     :designed-by  {:value "ME!!!!!"}
+     :date         {:value "Today"}
+     :version      {:value "7"}
+
+     :sections
+     {:key-partners
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :key-activities
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :key-resources
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :value-propositions
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :customer-relationships
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :channels
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :customer-segments
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :cost-structure
+      [{:value "Item 1"}
+       {:value "Item 2"}]
+
+      :revenue-streams
+      [{:value "Item 1"}
+       {:value "Item 2"}]}}))
+
+(om/root doc root js/document.body)
